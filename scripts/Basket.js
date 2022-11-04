7b5211dfeb54b5c7bfb25bc8994a585ba064bd11
Adam Dylus <a.adamdylus@gmail.com>
2022-11-04 20:25:00 +0100

    v003

--- scripts/Basket.js
+++ scripts/Basket.js
@@ -23,7 +23,10 @@ class Basket {
   }
 
   getProductsFromLocaleStorage() {
-    this.products = JSON.parse(localStorage.getItem("basket"));
+    // this.products = JSON.parse(localStorage.getItem("basket"));
+    if (JSON.parse(localStorage.getItem("basket")) === null) {
+      this.products = [];
+    }
   }
 
   getTotalValue() {
